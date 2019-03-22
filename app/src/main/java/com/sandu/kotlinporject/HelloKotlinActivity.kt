42d96aa39f3c7d03a892646ecb2ce379f186d5c4
aibfive <1456574276@qq.com>
2019-03-22 18:23:37 +0800

    1、Kotlin学习中；

---
+++ app/src/main/java/com/sandu/kotlinporject/HelloKotlinActivity.kt
@@ -0,0 +1,33 @@
+package com.sandu.kotlinporject
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_hello_kotlin.*
+
+/**
+ * KotlinPorject
+ * @author lizewu
+ * @date 2019/3/22
+ */
+class HelloKotlinActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello_kotlin)
+        setOnClickListener()
+    }
+
+    /**
+     * 设置点击监听器
+     */
+    fun setOnClickListener(){
+        backIv.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            backIv.id -> finish()  //关闭页面
+        }
+    }
+}
---
+++ app/src/main/java/com/sandu/kotlinporject/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.sandu.kotlinporject
+
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*;
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+/**
+ * KotlinPorject
+ * @author lizewu
+ * @date 2019/3/22
+ */
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+        setOnClickListener()
+    }
+
+    /**
+     * 设置监听事件
+     */
+    fun setOnClickListener(){
+        helloKotlinTv.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            helloKotlinTv.id -> openHelloKotlinActivity()
+        }
+    }
+
+    /**
+     * 打开HelloKolin页面
+     */
+    fun openHelloKotlinActivity(){
+        var intent = Intent()
+        intent.setClass(this, HelloKotlinActivity::class.java)
+        startActivity(intent)
+    }
+
+}
